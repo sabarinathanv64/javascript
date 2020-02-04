4a8780fcdeb71aa247bfd8e117e5fce6386ce024
sabarinathanv64 <60459542+sabarinathanv64@users.noreply.github.com>
2020-02-04 11:48:27 +0530

    Create reverse.js

---
+++ reverse.js
@@ -0,0 +1,12 @@
+try{
+    console.log("MULTIPLICATION TABLE");
+    var arr=[23,64,11,54,12];
+    var s=arr.reverse();
+    console.log(s);
+        
+        
+    
+}  
+catch(ee){
+    console.log(ee.message);
+}
