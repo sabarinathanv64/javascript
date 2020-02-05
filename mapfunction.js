30667138e19fb2e003e6a85749fb90ad33d10025
sabarinathanv64 <60459542+sabarinathanv64@users.noreply.github.com>
2020-02-05 12:07:09 +0530

    Create mapfunction.js

---
+++ mapfunction.js
@@ -0,0 +1,12 @@
+try{
+var numbers = [65, 44, 12, 4];
+var newarray = numbers.map(myFunction)
+
+function myFunction(num) {
+  return num * 10;
+}
+console.log(newarray);
+}
+catch(ee){
+    console.log(ee.message);
+}
